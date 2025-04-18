c2c45596847ea9c0a707a53a53b4f49ec50117be
陳謙 <t112598078@ntut.edu.tw>
2025-04-18 19:53:09 +0800

    Change the Docker Compose command from "docker-compose" into "docker compose"
    Signed-off-by: 陳謙 <t112598078@ntut.edu.tw>

--- src/main/java/server_instance/SpringPetclinicServer.java
+++ src/main/java/server_instance/SpringPetclinicServer.java
@@ -66,7 +66,7 @@ public class SpringPetclinicServer extends ServerInstanceManagement {
     }
 
     private String findBusyProcess() {
-        String containerID = CommandHelper.executeCommand("docker-compose", "-f", compose_file, "ps", "-q");
+        String containerID = CommandHelper.executeCommand("docker", "compose", "-f", compose_file, "ps", "-q");
         System.out.println("find the container id is :" + containerID);
         return containerID;
     }
@@ -110,7 +110,7 @@ public class SpringPetclinicServer extends ServerInstanceManagement {
 
     private void createServer() {
         long startTime = System.nanoTime();
-        CommandHelper.executeCommand("docker-compose", "-f", compose_file, "up", "-d");
+        CommandHelper.executeCommand("docker", "compose", "-f", compose_file, "up", "-d");
         long endTime = System.nanoTime();
         double timeElapsed = (endTime - startTime) / 1000000000.0;
         System.out.println("\nServer Port is " + server_port + ", Starting server instance waiting time is :" + timeElapsed);
@@ -131,7 +131,7 @@ public class SpringPetclinicServer extends ServerInstanceManagement {
                     throw new RuntimeException();
                 }
             }
-            CommandHelper.executeCommand("docker-compose", "-f", compose_file, "rm", "-svf");
+            CommandHelper.executeCommand("docker", "compose", "-f", compose_file, "rm", "-svf");
         }
         long endTime = System.nanoTime();
         double timeElapsed = (endTime - startTime) / 1000000000.0;
