b26ed05727cfb136d0b5469f722a370167565aa7
陳謙 <t112598078@ntut.edu.tw>
2025-04-18 19:53:09 +0800

    Change the Docker Compose command from "docker-compose" into "docker compose"
    Signed-off-by: 陳謙 <t112598078@ntut.edu.tw>

--- src/main/java/server_instance/NodeBBServer.java
+++ src/main/java/server_instance/NodeBBServer.java
@@ -69,7 +69,7 @@ public class NodeBBServer extends ServerInstanceManagement {
     }
 
     private String findBusyProcess() {
-        String containerID = CommandHelper.executeCommand("docker-compose", "-f", compose_file, "ps", "-q");
+        String containerID = CommandHelper.executeCommand("docker", "compose", "-f", compose_file, "ps", "-q");
         System.out.println("find the container id is :" + containerID);
         return containerID;
     }
@@ -119,7 +119,7 @@ public class NodeBBServer extends ServerInstanceManagement {
 
     private void createServer() {
         long startTime = System.nanoTime();
-        CommandHelper.executeCommand("docker-compose", "-f", compose_file, "up", "-d");
+        CommandHelper.executeCommand("docker", "compose", "-f", compose_file, "up", "-d");
         long endTime = System.nanoTime();
         double timeElapsed = (endTime - startTime) / 1000000000.0;
         System.out.println("\nServer Port is " + server_port + ", Waiting time is :" + timeElapsed);
@@ -128,7 +128,7 @@ public class NodeBBServer extends ServerInstanceManagement {
     @Override
     public void closeServerInstance() {
         long startTime = System.nanoTime();
-        CommandHelper.executeCommand("docker-compose", "-f", compose_file, "rm", "-svf");
+        CommandHelper.executeCommand("docker", "compose", "-f", compose_file, "rm", "-svf");
         long endTime = System.nanoTime();
         double timeElapsed = (endTime - startTime) / 1000000000.0;
         System.out.println("\nServer Port is " + server_port + ", Closing server instance waiting time is :" + timeElapsed);
